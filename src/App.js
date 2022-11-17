213144eb430897460e631c575e1b0137758d5581
XAL-AMLX <amaljiths612@gmail.com>
2022-11-17 09:51:29 +0530

    add nasa-image

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import { useState } from 'react';
 
 function App() {
+
+  const [imgUrl, setImgUrl] = useState(null);
+
+  const currentDate = new Date()
+  console.log(currentDate)
+
+  axios({
+    method: "get",
+    url: "https://api.nasa.gov/planetary/apod?api_key=xFXxvPXqX7C1TlFeHZJR0blXBKoNINgK4wFr2iZB",
+    responseType: "json",
+  }).then(function (response) {
+    console.log(setImgUrl(response.data["url"]));
+  });
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" className='input-url'/>
+      <div className='response-div'>
+        <img src={imgUrl} alt="nasa-image"/>
+      </div>
     </div>
   );
 }
